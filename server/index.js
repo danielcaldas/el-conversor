5020612f6055fc2b3a236cdfa48d4d61ac6a3355
danielcaldas <caldasjdaniel@gmail.com>
2018-03-20 23:39:14 +0000

    Add tests setup mocha+chai and nyc coverage

--- server/index.js
+++ server/index.js
@@ -16,9 +16,11 @@ app.get('/', (request, response) => {
 });
 
 app.get('/convert/:number', (request, response) => {
-    response.status(200).send(`convert ${request.params.number}`);
+    response.status(200).send({data: `convert ${request.params.number}`});
 });
 
 app.listen(app.get('port'), function() {
     console.log(`node server is running on port with route http://localhost:${app.get('port')}`);
 });
+
+export { app };
---
+++ server/test/index.test.js
@@ -0,0 +1,21 @@
+import chai from 'chai';
+import chaiHttp from 'chai-http';
+import { app as server } from '../index';
+
+const expect = chai.expect;
+
+chai.use(chaiHttp);
+
+describe('index', () => {
+    describe('GET /convert/:number', () => {
+        it('it should get result of number conversion', (done) => {
+            chai.request(server)
+                .get('/convert/23')
+                .end((err, res) => {
+                    expect(res.status).to.equal(200);
+                    expect(res.body.data).to.equal('convert 23');
+                    done();
+                });
+        });
+    });
+});
