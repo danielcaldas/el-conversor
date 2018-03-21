4b500e313c6b35ebe3e2344946e6f9878ce7422e
danielcaldas <caldasjdaniel@gmail.com>
2018-03-21 20:37:51 +0000

    Avoid app to listen twice :star: with module.parent

--- server/src/index.js
+++ server/src/index.js
@@ -18,9 +18,11 @@ app.get('/convert/:number', (request, response) => {
     response.status(200).send({data: `convert ${request.params.number}`});
 });
 
-app.listen(app.get('port'), function() {
-    /*eslint no-console: 0*/
-    console.log(`node server is running on port with route http://localhost:${app.get('port')}`);
-});
+if (!module.parent) {
+    app.listen(app.get('port'), function() {
+        /*eslint no-console: 0*/
+        console.log(`node server is running on port with route http://localhost:${app.get('port')}`);
+    });
+}
 
 export { app };
