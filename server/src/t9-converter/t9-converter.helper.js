0e1633161f5666a03bfe5385e43cef7fc446a51a
danielcaldas <caldasjdaniel@gmail.com>
2018-03-21 21:26:19 +0000

    Small improvements in t9-converter.helper

--- server/src/t9-converter/t9-converter.helper.js
+++ server/src/t9-converter/t9-converter.helper.js
@@ -18,13 +18,12 @@ function appendLetterToWords(words, letter) {
 }
 
 /**
- * @TODO: Improve fn: remove for loop, maybe split into smaller functions
  * For a given digit maps its correspondent characters and appends
  * them to all given words. If no words are provided it simply returns
  * array of characters for given digit.
  * @param {Array.<string>} words array of words.
  * @param {number|string} digit digit to be mapped into letters and appended to each word in words.
- * @returns {<Array.<string>} news words resulting from the append operation.
+ * @returns {Array.<string>} news words resulting from the append operation.
  */
 function expandWordsForDigit(words = [], digit) {
     if (!digit) {
@@ -35,17 +34,9 @@ function expandWordsForDigit(words = [], digit) {
         return PHONE_NUM_LETTER_MAPPING[digit]
     }
 
-    let newWords = [];
     const letters = PHONE_NUM_LETTER_MAPPING[digit];
-    const nLetters = letters.length;
 
-    for (let i = 0; i < nLetters; i++) {
-        const letter = letters[i];
-
-        newWords = newWords.concat(appendLetterToWords(words, letter));
-    }
-
-    return newWords;
+    return letters.reduce((acc, letter) => acc.concat(appendLetterToWords(words, letter)), []);
 }
 
 export { expandWordsForDigit };
