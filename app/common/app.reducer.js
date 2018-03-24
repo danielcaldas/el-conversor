fa2cb9b0460bce1ae0eb7a2fd1fea56661f44c0f
danielcaldas <caldasjdaniel@gmail.com>
2018-03-24 23:19:17 +0000

    Implement reducer

--- app/common/app.reducer.js
+++ app/common/app.reducer.js
@@ -1,13 +1,26 @@
+import { ACTIONS } from './app.const';
+
 const defaultState = {
     fetching: false,
     error: null,
-    user: null,
-    token: null
+    words: null
 };
 
 export default function reducer(state = defaultState, action) {
     switch (action.type) {
-        // @TODO
+        case ACTIONS.CONVERT_PENDING: {
+            return { ...state, fetching: true };
+        }
+        case ACTIONS.CONVERT_REJECTED: {
+            const msg = action.payload || '';
+
+            return { ...state, error: msg };
+        }
+        case ACTIONS.CONVERT_FULFILLED: {
+            const words = action.payload;
+
+            return { fetching: false, error: null, words };
+        }
     }
 
     return state;
