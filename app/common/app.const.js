d7e9ce26396340cdc3902ca953bf3ea60d6bf8bc
danielcaldas <caldasjdaniel@gmail.com>
2018-03-24 23:18:54 +0000

    Add action for convert num to word

---
+++ app/common/app.const.js
@@ -0,0 +1,11 @@
+/**
+ * CONVERT - user demands that current inputted number to be converter.
+ */
+const ACTIONS = {
+    CONVERT: 'CONVERT',
+    CONVERT_PENDING: 'CONVERT_PENDING',
+    CONVERT_REJECTED: 'CONVERT_REJECTED',
+    CONVERT_FULFILLED: 'CONVERT_FULFILLED'
+};
+
+export { ACTIONS };
