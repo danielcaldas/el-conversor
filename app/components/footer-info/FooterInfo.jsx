f401ba5382e1a0c7175a993ccd2cfb14c71cc20a
danielcaldas <caldasjdaniel@gmail.com>
2018-03-25 14:51:44 +0100

    Fix file-loader setup

--- app/components/footer-info/FooterInfo.jsx
+++ app/components/footer-info/FooterInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import './footer-info.scss';
+import githubLogo from '../../assets/github-logo.png';
 
 export default class FooterInfo extends React.Component {
     render() {
@@ -8,7 +9,7 @@ export default class FooterInfo extends React.Component {
             <div className="footer-info">
                 <div className="footer-info__refs">
                     <a className="footer-info__github" href="https://github.com/danielcaldas/el-conversor">
-                        <img src="../../assets/github-logo.png"></img>
+                        <img src={githubLogo}></img>
                     </a>
                 </div>
                 <span className="footer-info__title">El Conversor</span>
