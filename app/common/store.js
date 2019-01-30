0123bb88387b381fa87faac837d6451361dc833a
Daniel Caldas <11733994+danielcaldas@users.noreply.github.com>
2019-01-30 22:24:00 +0100

    Feature/setup vscode debugging (#14)
    * Update dependencies
    
    * Integrate composeWithDevTools in store.js
    
    * Add .vscode config with js debugger

--- app/common/store.js
+++ app/common/store.js
@@ -1,6 +1,10 @@
-import { applyMiddleware, createStore } from 'redux';
+import {
+    createStore,
+    applyMiddleware,
+} from 'redux';
 import { createLogger } from 'redux-logger';
 import promise from 'redux-promise-middleware';
+import { composeWithDevTools } from 'remote-redux-devtools';
 
 import reducer from './reducers';
 
@@ -10,4 +14,11 @@ const middleware = ENV.NAME === 'production'
     ? applyMiddleware(promise())
     : applyMiddleware(promise(), createLogger());
 
-export default createStore(reducer, middleware);
+const composeEnhancers = composeWithDevTools({
+    realtime: true,
+    name: 'store',
+    host: 'localhost',
+    port: 1024,
+});
+
+export default createStore(reducer, {}, composeEnhancers(middleware));
